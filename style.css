e92fec4ce98a8e3d1bcbe27613273981437a0bd1
CHFOUR4 <design02@chfourenergy.com>
2026-01-12 17:58:58 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,38 @@
+body {
+  font-family: Arial;
+  background: #f2f2f2;
+  padding: 20px;
+}
+
+h1, h2, h3 {
+  text-align: center;
+}
+
+#project-form, #projectDetails {
+  max-width: 500px;
+  margin: auto;
+  background: white;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+input, textarea {
+  width: 100%;
+  padding: 8px;
+  margin: 5px 0;
+}
+
+button {
+  margin-top: 5px;
+  padding: 8px;
+  width: 100%;
+  cursor: pointer;
+}
+
+li {
+  list-style: none;
+  background: #eee;
+  margin: 5px 0;
+  padding: 5px;
+  cursor: pointer;
+}
